9cd7c85b76e8d7d9575357cfd46d75d6258666bd
bandrews <b9andrews@gmail.com>
2025-06-02 02:20:29 -0400

    get weather data

--- weather-dashboard.py
+++ weather-dashboard.py
@@ -1,10 +1,13 @@
 from textual.app import App
 from textual.widgets import Static
+from src.weather.get_weather import get_weather
 
 
 class WeatherDashboard(App):
     def compose(self):
+        weather_data = get_weather().json()
         yield Static("Hello, Textual!")
+        yield Static(f"{weather_data["latitude"]}")
 
 
 if __name__ == "__main__":
