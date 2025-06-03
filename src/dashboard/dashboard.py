0c61b5ba855558148574c678777c6acbf5da2b20
bandrews <b9andrews@gmail.com>
2025-06-02 23:32:27 -0400

    start with forecast container

--- src/dashboard/dashboard.py
+++ src/dashboard/dashboard.py
@@ -1,6 +1,6 @@
 from textual.app import App
-from textual.widgets import Static
-from textual.containers import Grid, Container
+from textual.widgets import Static, Header
+from textual.containers import Grid, Container, Horizontal
 from src.weather.weather import Weather
 from src.dashboard.image_widget import ImageWidget
 
@@ -14,9 +14,11 @@ class WeatherDashboard(App):
         super().__init__()
 
     def compose(self):
+        yield Header("Custom")
         with Grid(id="app-grid"):
             # Current Weather Container
             with Container(classes="box-two-row"):
+                yield Static("Current Weather", classes="center-text")
                 yield Static(f"Last Updated: {self.weather_obj.weather["current"]["time"].split("T")[0]} {self.weather_obj.weather["current"]["time"].split("T")[1]}")
                 yield Static(f"Temperature / Feels Like: {self.weather_obj.weather["current"]["temperature_2m"]} {self.weather_obj.weather["current_units"]["temperature_2m"]} / {self.weather_obj.weather["current"]["apparent_temperature"]} {self.weather_obj.weather["current_units"]["temperature_2m"]}")
                 yield Static(f"Humidity: {self.weather_obj.weather["current"]["relative_humidity_2m"]} {self.weather_obj.weather["current_units"]["relative_humidity_2m"]}")
@@ -32,7 +34,13 @@ class WeatherDashboard(App):
 
             # Forecast Container
             with Container(classes="box-two-col"):
-                yield Static("Daily Forecast Box")
+                yield Static("Seven Day Forecast", classes="center-text")
+                with Horizontal():
+                    yield Static(f'first one')
+                    # below doesn't show up
+                    yield Static(f'second one')
+                    yield Static(f'third one')
+
 
     def on_mount(self):
         pass
