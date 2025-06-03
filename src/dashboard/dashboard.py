e106196a317adc87663de658a07449cca6ca7646
bandrews <b9andrews@gmail.com>
2025-06-02 22:11:31 -0400

    moved weather to class so that object is extensible

--- src/dashboard/dashboard.py
+++ src/dashboard/dashboard.py
@@ -1,6 +1,6 @@
 from textual.app import App
 from textual.widgets import Static
-from src.weather.get_weather import get_weather
+from src.weather.weather import Weather
 from src.dashboard.image_widget import ImageWidget
 
 
@@ -9,11 +9,11 @@ class WeatherDashboard(App):
 
     def __init__(self, testkw=False):
         self.testkw = testkw
-        self.weather_data = get_weather(testkw=self.testkw)
+        self.weather_obj = Weather(testkw=self.testkw)
         super().__init__()
 
     def compose(self):
-        yield Static(f"{self.weather_data["latitude"]}", classes="box", id="two-row")
+        yield Static(f"{self.weather_obj.weather["latitude"]}", classes="box", id="two-row")
         yield ImageWidget("images/winter-images/1.jpg", (30, 30))
         yield Static("yeah whatever")
         yield Static("Daily Forecast Box", classes="box", id="two-column")
