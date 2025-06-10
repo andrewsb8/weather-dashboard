fc72dce000a381d896a0eefdcf830a49fc1c10ee
bandrews <b9andrews@gmail.com>
2025-06-09 22:40:01 -0400

    template logic for retrieving images

--- src/weather/weather.py
+++ src/weather/weather.py
@@ -19,3 +19,12 @@ class Weather(object):
             return get(
                 "https://api.open-meteo.com/v1/forecast?latitude=39.92026&longitude=-75.15935&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_probability_max,precipitation_sum,precipitation_hours&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,wind_speed_10m,wind_direction_10m,precipitation,rain,snowfall,weather_code,cloud_cover&timezone=America%2FNew_York&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch"
             ).json()
+
+    def get_current_weather_img_path(self):
+        pass
+
+    def get_forecast_img_paths(self):
+        pass
+
+    def get_weather_img_path(self):
+        pass
